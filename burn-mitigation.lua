db0be4b9251b81a7f1c74ef2f9c9999568891f25
retrotails <10767978+retrotails@users.noreply.github.com>
2021-10-03 02:44:08 -0400

    minor cleanup, changes

--- burn-mitigation.lua
+++ burn-mitigation.lua
@@ -1,40 +1,37 @@
---minutes per complete swipe
-speed = 120
+speed = 120 --minutes per complete swipe
+freq = 120 -- update frequency (Hz)
 
 tau=math.pi*2
-sin=math.sin
 offset = 0
-monitor_aspect = 0
-monitor_w = 0
-monitor_h = 0
+display_aspect = 0
+display_w, display_h = 0,0
 content_aspect = 0
 speed=((1/speed)/60)*tau
 
 function update_aspect(name, value)
 	if value > 0 then
-		if name == "osd-width"  then monitor_w = value end
-		if name == "osd-height" then monitor_h = value end
+		if name == "osd-width"  then display_w = value end
+		if name == "osd-height" then display_h = value end
 	end
-	if monitor_w > 0 and monitor_h > 0 then
-		monitor_aspect = monitor_w/monitor_h
+	if display_w > 0 and display_h > 0 then
+		display_aspect = display_w/display_h
 	end
 end
 mp.observe_property("osd-width", "number", update_aspect)
 mp.observe_property("osd-height", "number", update_aspect)
 
-function thing(what)
+function thing()
 	if content_aspect == 0 then
-		local aspect = mp.get_property("video-params/aspect")
-		if aspect then content_aspect = aspect end
-	elseif monitor_aspect > 0 then
-		offset = (offset + speed*0.01)%tau
-		if (content_aspect/monitor_aspect) > 1 then
-			mp.set_property_number("video-pan-y", sin(offset)*(1 - (content_aspect/monitor_aspect))*.5)
+		content_aspect = mp.get_property("video-params/aspect") or 0
+	elseif display_aspect > 0 then
+		offset = (offset + speed/freq)%tau
+		if (content_aspect/display_aspect) > 1 then
+			mp.set_property_number("video-pan-y", math.sin(offset)*(1 - (content_aspect/display_aspect))*.5)
 			mp.set_property_number("video-pan-x", 0)
 		else
-			mp.set_property_number("video-pan-x", sin(offset)*(1 - (monitor_aspect/content_aspect))*.5)
+			mp.set_property_number("video-pan-x", math.sin(offset)*(1 - (display_aspect/content_aspect))*.5)
 			mp.set_property_number("video-pan-y", 0)
 		end
 	end
 end
-mp.add_periodic_timer(.01, thing)
+mp.add_periodic_timer(1/freq, thing)
